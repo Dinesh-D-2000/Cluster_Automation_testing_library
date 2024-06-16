39bdb994a3cab036c17fa78830a19bad6e615b3f
Dinesh-D-2000 <dineshdeena488@gmail.com>
2024-06-16 19:40:05 +0530

    changes done

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description='Library for automation testing of digital instrument clusters.',
     author='Dinesh-D-2000',
     author_email='dineshdeena488@gmail.com',
-    url="https://github.com/Dinesh-D-2000/test_lib",
+    url="https://github.com/Dinesh-D-2000/Cluster_Automation_testing_library/tree/main/test_lib",
     packages=find_packages(include=["test_lib", "test_lib.*"]),
     include_package_data=True,
 )
