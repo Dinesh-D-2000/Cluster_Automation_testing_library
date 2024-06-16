4a53a53673f0f705a568246b1c03c5531fa1385e
Dinesh-D-2000 <dineshdeena488@gmail.com>
2024-06-16 19:51:37 +0530

    changes done

--- setup.py
+++ setup.py
@@ -1,15 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='test_lib',
+    name='Cluster_Automation_testing_library',
     use_scm_version=True,
     description='Library for automation testing of digital instrument clusters.',
     author='Dinesh-D-2000',
     author_email='dineshdeena488@gmail.com',
-    url="https://github.com/Dinesh-D-2000/Cluster_Automation_testing_library/tree/main/test_lib",
+    url="https://github.com/Dinesh-D-2000/Cluster_Automation_testing_library",
     packages=find_packages(include=["test_lib", "test_lib.*"]),
     include_package_data=True,
 )
-
-print(find_packages(include=["test_lib", "test_lib.*"]))
-
