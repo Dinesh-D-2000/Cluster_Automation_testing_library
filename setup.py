568c18848da1b3366c847f17d2e04cafe262f608
Dinesh D <109975786+Dinesh-D-2000@users.noreply.github.com>
2024-06-16 19:05:01 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Cluster_Automation_testing_library',
-    version='1.0.0',
+    use_scm_version=True,
     description='Library for automation testing of digital instrument clusters.',
     author='Dinesh-D-2000',
     author_email='dineshdeena488@gmail.com',
