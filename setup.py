b4614dc410071dbc53292a54b55d5576d00aeef7
Dinesh D <109975786+Dinesh-D-2000@users.noreply.github.com>
2024-06-16 16:17:39 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Cluster_Automation_testing_library',
+    version='1.0.0',
+    description='Library for automation testing of digital instrument clusters.',
+    author='Dinesh-D-2000',
+    author_email='dineshdeena488@gmail.com',
+    packages=find_packages(),
+)
